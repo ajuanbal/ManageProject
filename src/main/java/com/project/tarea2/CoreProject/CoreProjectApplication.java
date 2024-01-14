4f256f9f1fa1635d8e9298a6dd10ad103e1346a4
ajuanbal <juanbalderramo9@gmail.com>
2024-01-14 18:53:31 -0500

    first commit

---
+++ src/main/java/com/project/tarea2/CoreProject/CoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.tarea2.CoreProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/tarea2/CoreProject/CoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tarea2.CoreProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
